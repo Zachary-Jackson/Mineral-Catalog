9badd2808d5f0352d35c37a66bcaf8a192171ac1
Zachary-Jackson <jackson.zachary.r@gmail.com>
2017-12-06 13:47:12 -0500

    CSS linked to minerals app.
    The Mineral Catalog's homepage and detail page are now showing. Some of the html needs sorted and tests are to be added.

---
+++ mineral_catalog/minerals/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'detail/(?P<pk>\d+)/$', views.detail, name='detail'),
+]
--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Mineral
 
 
 def home(request):
     '''This is the homepage for the minerals app.'''
-    return render(request, 'minerals/layout.html')
+    mineral = Mineral.objects.all()
+    return render(request, 'minerals/mineral_list.html', {'minerals': mineral})
+
+
+def detail(request, pk):
+    '''This is the detail page for the minerals app.'''
+    mineral = get_object_or_404(Mineral, pk=pk)
+    return render(request, 'minerals/mineral_detail.html', {'mineral':
+                                                            mineral})
